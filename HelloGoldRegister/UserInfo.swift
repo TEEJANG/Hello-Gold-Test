c25e3e3ce8244d48afac87cf80f8758aadc2c1d9
Teerawat <teerawat.jangkrajang@gmail.com>
2019-01-28 14:35:32 +0700

    test

---
+++ HelloGoldRegister/UserInfo.swift
@@ -0,0 +1,9 @@
+//
+//  UserInfo.swift
+//  HelloGoldRegister
+//
+//  Created by Teerawat on 26/1/2562 BE.
+//  Copyright © 2562 Teerawat. All rights reserved.
+//
+
+import Foundation
---
+++ HelloGoldRegister/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloGoldRegister
+//
+//  Created by Teerawat on 26/1/2562 BE.
+//  Copyright © 2562 Teerawat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
